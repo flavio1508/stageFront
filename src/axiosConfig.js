5a15dd5974588a3ae5fb2589962ffef07a6a10bb
Flávio Augusto <100893001+flavio1508@users.noreply.github.com>
2025-06-08 03:25:08 -0300

    first commit

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// Configuração global do axios para incluir cookies nas solicitações
-// axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 
 export default axios;
