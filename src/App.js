8ffe3f805ae7ba1a61b3b9ef5b025cb06c2649cb
Flávio Augusto <100893001+flavio1508@users.noreply.github.com>
2025-06-09 01:48:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ProjectsSimple from "./Components/pages/ProjectsSimple"; // Ajuste o caminho para o componente principal
-import Login from "./Components/Login/Login"; // Ajuste o caminho para o componente de login
-import Profile from "./Components/Profile/Profile"; // Ajuste o caminho para o componente de perfil
-import Payroll from "./Components/Payroll/Payroll"; // Ajuste o caminho para o componente de folha de pagamento
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ProjectsSimple from "./Components/pages/ProjectsSimple";
+import Login from "./Components/Login/Login";
+import Profile from "./Components/Profile/Profile";
+import Payroll from "./Components/Payroll/Payroll";
+import { Routes, Route } from "react-router-dom"; // 🔄 tire o BrowserRouter daqui
 import NewUser from "./Components/NewUser/NewUser";
 import ProjectDetails from "./Components/ProjectsDetails/ProjectsDetails";
 import Register from "./Components/Register/Register";
@@ -15,19 +15,17 @@ Modal.setAppElement("#root");
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/projects" element={<ProjectsSimple />} />
-        <Route path="/newUser" element={<NewUser />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/payroll" element={<Payroll />} />
-        <Route path="/project/:projectId" element={<ProjectDetails/>}/>
-        <Route path="/history" element={<History />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/projectStatus/:projectId" element={<ProjectStatus/>}/>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/projects" element={<ProjectsSimple />} />
+      <Route path="/newUser" element={<NewUser />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/payroll" element={<Payroll />} />
+      <Route path="/project/:projectId" element={<ProjectDetails />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/projectStatus/:projectId" element={<ProjectStatus />} />
+    </Routes>
   );
 }
 
